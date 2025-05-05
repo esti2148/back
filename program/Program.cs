b0337b931ef6473f7a94ec8870964be0e4097354
esti2148 <ee214800575@sgur.org.il>
2025-05-05 15:14:20 +0300

    Add files via upload

---
+++ program/Program.cs
@@ -0,0 +1,16 @@
+﻿using Bl;
+using Bl.Models;
+using Bl.Services;
+
+namespace program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+           
+        }
+    }
+}
